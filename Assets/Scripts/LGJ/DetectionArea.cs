6e213be334fe2a44bd5a6b85f4d8c25c007e48e4
Remi Alkemade <remialkemade@gmail.com>
2015-04-04 03:28:26 +0200

    Added DetectionArea script. Started Gate animations and endgame triggers.

---
+++ Assets/Scripts/LGJ/DetectionArea.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface AreaListener {
+	void OnAreaEnter(DetectionArea area, Collider2D collider);
+	void OnAreaExit(DetectionArea area, Collider2D collider);
+}
+
+public class DetectionArea : MonoBehaviour {
+
+	List<AreaListener> areaListeners = new List<AreaListener>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddBinListener(AreaListener listener) {
+		areaListeners.Add(listener);
+	}
+
+	void OnTrigger2DEnter(Collider2D collider) {
+		foreach(AreaListener listener in areaListeners) {
+			listener.OnAreaEnter(this, collider);
+		}
+	}
+}
