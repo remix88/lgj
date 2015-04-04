1d3f23df0dd5378f24a84f70662a0cef1c7d9943
Remi Alkemade <remialkemade@gmail.com>
2015-04-04 02:00:53 +0200

    Projectiles are destroyed from a set distance

--- Assets/Scripts/LGJ/CleanInvisible.cs
+++ Assets/Scripts/LGJ/CleanInvisible.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class CleanInvisible : MonoBehaviour {
 
+	public static float DistanceFromPlayer = 30f;
+
+	private GameObject Knight;
+
 	// Use this for initialization
 	void Start () {
-	
+
+	}
+
+	void Awake() {
+		Knight = GameObject.Find("Knight");
 	}
 	
 	// Update is called once per frame
@@ -16,6 +24,13 @@ public class CleanInvisible : MonoBehaviour {
 	void OnBecameInvisible() {
 		if(gameObject.activeSelf) {
 			Destroy (gameObject);
+		}
+	}
+
+	void OnEnable()
+	{
+		if(Mathf.Abs(Knight.transform.position.x - transform.position.x) > DistanceFromPlayer) {
+			Destroy(gameObject);
 		}
 	}
 }
